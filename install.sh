141a7750a188522726a982fae756c5bd97630b80
lightning283 <deadtarget283@gmail.com>
2021-07-13 22:00:51 +0530

    upload files

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Standalone Python Written Lolcat Thanks To Tehmaze"
+sleep 1
+#########################################################################
+if [[ -d /usr/ ]]
+then
+sudo cp ~/zap/packages/lolcat/lolcat /bin/
+sudo chmod +x /bin/lolcat
+fi
+##########################################################################
+if [[ -d /sdcard/ ]]
+then
+cp /system/zap/packages/lolcat/lolcat /system/bin/
+chmod +x /system/bin/lolcat
+ln -s /system/bin/lolcat /bin/lolcat
+echo "Sucessfully Installed!!"
+fi
---
+++ uninstall.sh
@@ -0,0 +1,19 @@
+if [[ -d "/usr/" ]]
+then
+echo "Removing File From /bin"
+sudo rm -rf /bin/lolcat
+echo "Removing Downloaded Package"
+rm -rf ~/zap/packages/lolcat
+echo "Removed Succesfully"
+fi
+################################################################
+if [[ -d "/sdcard/" ]]
+then
+echo "Removing Downloaded Packge From Packages"
+rm -rf /system/packages/lolcat
+echo "Removing File from /bin"
+rm -rf /system/bin/lolcat
+echo "Removing Linked Files.."
+rm -rf /bin/lolcat
+echo "Uninstall Successfull.."
+fi
